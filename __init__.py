4a62f42b6cd6bc0a0e516efdfed933fe05c0a253
gbdngb12 <gbdngb12@naver.com>
2022-11-23 21:50:51 +0900

    add : __init__.py

---
+++ __init__.py
@@ -0,0 +1,59 @@
+from albert import *
+import os
+import re
+
+__title__ = "Linux manual"
+__version__ = "0.1.1"
+__triggers__ = "man "
+__authors__ = "dong"
+
+iconPath = iconLookup("terminal")
+
+manPath = '/usr/share/man/man' # default man1, man2, man3, ... , man9
+
+mandb = []
+
+def initialize() :
+    # tmp ['*.[1-9].gz', '*.[1-9].gz', ..]
+    tmp = os.listdir(manPath+str(1))
+    for i in range(2,9) :
+        tmp = list(set(tmp + os.listdir(manPath+ str(i))))
+    for s in tmp :
+        # remove File extension
+        mandb.append(re.sub(".[1-9].gz", "", s))
+    
+def defaultItem(query):
+    item = Item()
+    item.icon = iconPath
+    item.text = 'Linux Manual (man) %s' % query
+    item.subtext = 'Show Linux Manual'
+    item.completion = __triggers__
+    item.addAction(TermAction(text='show Manual',script="man %s" % query))
+    return item
+
+def search(query, result) :
+    
+    matching = [s for s in mandb if query.lower() in s.lower()]
+    if len(matching) == 0:
+        return result.append(defaultItem(query))
+    for match in matching :
+        item = Item()
+        item.icon = iconPath
+        item.text = 'man %s' % match
+        item.subtext = 'Show Linux Manual'
+        item.completion = 'man %s' % match
+        item.addAction(TermAction(text='show Manual',script="man %s" % match))
+        result.append(item)
+
+def handleQuery(query) :
+    if not query.isTriggered:
+        return
+    
+    results = []
+    if query.string == "" :    
+        results.append(defaultItem(query.string))
+        return results
+    else :
+        search(query.string, results)
+        return results
+
